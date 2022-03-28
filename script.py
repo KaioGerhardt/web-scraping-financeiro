827433891b50036b330e19b8657bb73efc569090
KaioGerhardt <gerhardt.kaio@gmail.com>
2022-03-28 11:05:52 -0300

    construindo

--- script.py
+++ script.py
@@ -1,21 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+from gui import *
 
 #MXL
 def fundo_mxrf11():
     #cotacao
-    #html_cotacao = requests.get("https://chart.fundsexplorer.com.br/chart/mxrf11/").content
-    #soup = BeautifulSoup(html_cotacao, "html.parser")
-    #cotacao = soup.find("span", class_="value")
-    #print(cotacao.string)
+    html_cotacao = requests.get("https://chart.fundsexplorer.com.br/chart/mxrf11/").content
+    soup = BeautifulSoup(html_cotacao, "html.parser")
+    cotacao = soup.find("span", class_="value")
+    cotacao_final = cotacao.string
+    print(cotacao_final)
 
-    #ultimo dividendo
-    html_dividendo = requests.get("https://www.fundsexplorer.com.br/funds/mxrf11").content
-    soup1 = BeautifulSoup(html_dividendo, "html.parser")
-    dividendo = soup1.find_all("div", class_="carousel-cell is-selected")
-    #dividendo1 = dividendo.next_element
-    print(dividendo.next_element)
+def fundo_knri11():
+    #cotacao
+    html_cotacao = requests.get("https://chart.fundsexplorer.com.br/chart/knri11/").content
+    soup = BeautifulSoup(html_cotacao, "html.parser")
+    cotacao = soup.find("span", class_="value")
+    cotacao_final = cotacao.string
+    #print(cotacao_final)
+    #fundo_knri11()
+    cotacao_knri11["text"] = cotacao_final
 
-    #tentando setar o caminho especifico do segundo elemento a ser analisado
 
-fundo_mxrf11()
+if (__name__ == "__main__"):
+    #testes
+    fundo_mxrf11()
+    fundo_knri11()
